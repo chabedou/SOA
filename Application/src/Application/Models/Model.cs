63f4af65fed45b24fadab321225b3f1332803d4d
gregoire lafont <gregoirelafont@live.fr>
2016-12-23 14:41:15 +0100

    trying to create a database

---
+++ Application/src/Application/Models/Model.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Models
+{
+    public class ResumesContext : DbContext
+    {
+        public ResumesContext(DbContextOptions<ResumesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Developer> Users { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<Skill> Resumes { get; set; }
+    }
+
+    public class Developer
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+    }
+
+    public class Skill
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Resume
+    {
+        public int Id { get; set; }
+        public int SkillId { get; set; }
+        public int DevelopperId { get; set; }
+    }
+}
