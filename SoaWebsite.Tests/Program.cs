d8407865dd9885bca7e4a7084faf0cd12c31ea74
gregoire lafont <gregoirelafont@live.fr>
2017-01-26 16:55:10 +0100

    #7 added AddSkill method test

---
+++ SoaWebsite.Tests/Program.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace SoaWebsite.Tests
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        
+        }
+    }
+}
--- SoaWebsite.Tests/WebTests/ControllersTests/DevelopersControllerTests.cs
+++ SoaWebsite.Tests/WebTests/ControllersTests/DevelopersControllerTests.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 using SoaWebsite.Web.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace SoaWebsite.Web.Tests
+namespace SoaWebsite.Tests
 {
     [TestFixture]
     public class DevelopersControllerTests
@@ -37,9 +37,40 @@ namespace SoaWebsite.Web.Tests
                 Assert.AreEqual(1, context.Developers.Count());
                 Assert.AreEqual("Toto", context.Developers.Single().FirstName);
                 Assert.AreEqual("Tata", context.Developers.Single().LastName);
+                //Assert.AreEqual(1, context.Developers.Single().ID);
             }
         }
 
-        
+        [Test]
+        public async Task AddSkillPythonToADeveloperWorksFine()
+        {
+            var options = new DbContextOptionsBuilder<DeveloperContext>()
+                .UseInMemoryDatabase(databaseName: "AddSkill_writes_to_database")
+                .Options;
+
+            // Run the test against one instance of the context
+            using (var context = new DeveloperContext(options))
+            {
+                var controller = new DevelopersController(context);
+                var developer = new Developer();
+                
+                developer.FirstName = "Toto";
+                developer.LastName = "Tata";
+                controller.Create(developer);
+
+                var skill = new Skill();
+                skill.Name = "Python";
+                await controller.AddSkill((int?) developer.ID, skill);
+            }
+
+            // Use a separate instance of the context to verify correct data was saved to database
+            using (var context = new DeveloperContext(options))
+            {
+                Assert.AreEqual(1, context.Skills.Count());
+                var developer = await context.Developers.Include(d => d.DeveloperSkills).ThenInclude(x => x.Skill)
+                                              .SingleOrDefaultAsync(m => m.ID == 1);
+                Assert.AreEqual("Python", developer.DeveloperSkills.Single().Skill.Name);
+            }
+        }
     }
 }
